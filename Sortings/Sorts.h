e3c534f6fe439d828b00e179c59bb6adc59ec2f5
Ant Kerf <nicelad47@gmail.com>
2020-09-10 16:42:59 +0700

    Update Sorts.h

--- Sortings/Sorts.h
+++ Sortings/Sorts.h
@@ -5,28 +5,28 @@ class Sorts
 public:
 
 	template <typename T>
-	static void buble(T& arr)
-	{
-		size_t cswap = 1;
-		while (cswap > 0)
-		{
-			cswap = 0;
-			for (size_t i = 0; i < arr.size() - 1; i++)
-			{
-
-				if (arr[i] > arr[i + 1])
-				{
-					arr[i] += arr[i + 1];
-					arr[i + 1] = arr[i] - arr[i + 1];
-					arr[i] -= arr[i + 1];
-					cswap++;
-				}
-			}
-		}
-	}
+	static void buble(T& arr);
 
 
 };
 
+template<typename T>
+inline void Sorts::buble(T& arr)
+{
+	size_t cswap = 1;
+	while (cswap > 0)
+	{
+		cswap = 0;
+		for (size_t i = 0; i < arr.size() - 1; i++)
+		{
 
-
+			if (arr[i] > arr[i + 1])
+			{
+				arr[i] += arr[i + 1];
+				arr[i + 1] = arr[i] - arr[i + 1];
+				arr[i] -= arr[i + 1];
+				cswap++;
+			}
+		}
+	}
+}
