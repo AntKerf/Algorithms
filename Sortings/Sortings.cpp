e8f1c4305fea85f594641ecd5882ad1960cdd050
Anton Lukyanov <nicelad47@gmail.com>
2020-09-15 13:30:37 +0700

    add cocktail sort

--- Sortings/Sortings.cpp
+++ Sortings/Sortings.cpp
@@ -10,7 +10,10 @@ int main()
 	std::vector<int> vec = { 0,5,1,56,2,123,3 };
 
 	//Пузырьковая сортировка
-	Sorts::Buble(vec);
+//	Sorts::Buble(vec);
+
+	//Шейкерная сортировка
+//	Sorts::Cocktail(vec);
 
 	//Сортировка вставками
 	Sorts::Insertion(vec);
--- Sortings/Sorts.h
+++ Sortings/Sorts.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 class Sorts
 {
@@ -8,17 +9,21 @@ public:
 	static void Buble(T& arr);
 
 	template <typename T>
+	static void Cocktail(T& arr);
+
+	template <typename T>
 	static void Insertion(T& arr);
+
 
 };
 
 template<typename T>
 inline void Sorts::Buble(T& arr)
 {
-	size_t cswap = 1;
-	while (cswap > 0)
+	bool swaped;
+	do
 	{
-		cswap = 0;
+		swaped = false;
 		for (size_t i = 0; i < arr.size() - 1; i++)
 		{
 
@@ -27,30 +32,68 @@ inline void Sorts::Buble(T& arr)
 				arr[i] += arr[i + 1];
 				arr[i + 1] = arr[i] - arr[i + 1];
 				arr[i] -= arr[i + 1];
-				cswap++;
+				swaped = true;
 			}
 		}
-	}
+	} while (swaped);
+}
+
+template<typename T>
+inline void Sorts::Cocktail(T& arr)
+{
+	size_t left = 0, right = arr.size()-1;
+	bool swaped;
+	do
+	{
+		swaped = false;
+		for (size_t i = left; i < right; i++)
+		{
+			if (arr[i] > arr[i + 1])
+			{
+				arr[i] += arr[i + 1];
+				arr[i + 1] = arr[i] - arr[i + 1];
+				arr[i] -= arr[i + 1];
+				swaped = true;
+			}
+		}
+		right--;
+		for (size_t i = right; i > left; i--)
+		{
+			if (arr[i] < arr[i - 1])
+			{
+				arr[i] += arr[i - 1];
+				arr[i - 1] = arr[i] - arr[i - 1];
+				arr[i] -= arr[i - 1];
+				swaped = true;
+			}
+		}
+		left++;
+	} while (swaped);
 }
 
 template<typename T>
 inline void Sorts::Insertion(T& arr)
 {
-	for (size_t i = 1; i < arr.size(); i++)
-	{
-		if (arr[i - 1] > arr[i])
+	bool swaped;
+	do {
+		swaped = false;
+		for (size_t i = 1; i < arr.size(); i++)
 		{
-			size_t j = i;
-			while (arr[i] < arr[j - 1] && j>0) j--;
-			if (j != i)
+			if (arr[i - 1] > arr[i])
 			{
-				auto tmpValue = arr[i];
-				for (size_t t = i; t >= j; t--)
+				size_t j = i;
+				while (arr[i] < arr[j - 1] && j>0) j--;//поиск места вставки
+				if (j != i)
 				{
-					arr[t] = arr[t - 1];
+					auto tmpValue = arr[i];
+					for (size_t t = i; t >= j; t--)//сдвиг элементов
+					{
+						arr[t] = arr[t - 1];
+					}
+					arr[j] = tmpValue;//вставка
+					swaped = true;
 				}
-				arr[j] = tmpValue;
 			}
 		}
-	}
+	} while (swaped);
 }
