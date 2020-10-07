34cfcaef3698e0deb74cbf959dc2e4a8a061a64f
Anton Lukyanov <nicelad47@gmail.com>
2020-10-07 15:43:52 +0700

    emplace new/delete on unique_ptr update visualisation hash delete ~hash()

--- Structurs/Ant_Hash.h
+++ Structurs/Ant_Hash.h
@@ -9,20 +9,15 @@ template <typename K, typename V>
 class Ant_Hash
 {
 private:
-	//_STD unique_ptr<K[]> keys; //unique_ptr don`t work in natvis???
-	K* keys;
-
 	size_t count;
 	size_t count_bucket;
 	size_t capacity_t;
 	size_t hash_func(_STD string key);
 	size_t hash_func(int key);
-
-	//_STD unique_ptr<Ant_List<V>[]> table;
-	Ant_List<V>* table;
+	_STD unique_ptr<K[]> keys;
+	_STD unique_ptr<Ant_List<V>[]> table;
 public:
 	Ant_Hash();
-	~Ant_Hash();
 	void reserve(int);
 	void add(K, V);
 	size_t size();
@@ -39,18 +34,8 @@ inline Ant_Hash<K, V>::Ant_Hash()
 	capacity_t(1),
 	count_bucket(0)
 {
-	//table = _STD make_unique<Ant_List<V>[]>(capacity_t);
-	table = new Ant_List<V>[capacity_t]();
-
-	//keys = _STD make_unique<K[]>(capacity_t);
-	keys = new K[capacity_t]();
-}
-
-template<typename K, typename V>
-inline Ant_Hash<K, V>::~Ant_Hash()
-{
-	delete[] table;
-	delete[] keys;
+	table = _STD make_unique<Ant_List<V>[]>(capacity_t);
+	keys = _STD make_unique<K[]>(capacity_t);
 }
 
 template<typename K, typename V>
@@ -69,11 +54,8 @@ template<typename K, typename V>
 inline void Ant_Hash<K, V>::reserve(int countItems)
 {
 	capacity_t = countItems;
-	//table = _STD make_unique<Ant_List<V>[]>(capacity_t);
-	table = new Ant_List<V>[capacity_t]();
-
-	//keys = _STD make_unique<K[]>(capacity_t);
-	keys = new K[capacity_t]();
+	table = _STD make_unique<Ant_List<V>[]>(capacity_t);
+	keys = _STD make_unique<K[]>(capacity_t);
 }
 
 template<typename K, typename V>
